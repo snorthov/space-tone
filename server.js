13abc2d22b6b7cb8ef7e7e182fe1a646eb6c8101
STEPHEN NORTHOVER <snorthov@ca.ibm.com>
2017-03-08 13:34:08 -0600

    check for VCAP_APP_PORT first

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get(GET_TONE, /* @callback */ function(req, res) {
 });
 
 // listen for requests on the host at a port
-var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+var port = process.env.VCAP_APP_PORT || process.env.PORT || 3000;
 app.listen(port, function() {
 	console.log('Server running on port: %d', port);
 });
