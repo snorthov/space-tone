68b8f2cd2a945bd52570ad65a807a540471a1e48
STEPHEN NORTHOVER <snorthov@ca.ibm.com>
2017-07-11 01:13:09 +0000

    implement sauce

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Who is in space?  How are well are the communicating?
+
+[![Deploy to Bluemix](https://bluemix.net/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/snorthov/space-tone.git)
---
+++ test.js
@@ -0,0 +1,54 @@
+try {
+	var dotenv = require('dotenv');
+	dotenv.load({silent: true});
+} catch (error) {
+	console.log(error);
+}
+
+var NAME = "space-tone";
+var TEST_URL = "https://" + NAME + ".mybluemix.net/";
+
+var webdriver = require('selenium-webdriver');
+var USER_NAME = process.env.SAUCE_USERNAME;
+var ACCESS_KEY = process.env.SAUCE_ACCESS_KEY;
+
+//TODO - Test on more plaforms ...
+var driver = new webdriver.Builder().
+	withCapabilities({
+		'browserName': 'chrome',
+		'platform': 'Windows XP',
+		'version': '43.0',
+		'username': USER_NAME,
+		'accessKey': ACCESS_KEY,
+		'name': NAME
+	}).
+	usingServer("http://" + USER_NAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub").
+	build();
+
+//TODO - Rewrite to use Mocha ...
+var TITLE = 'Space';
+driver.getSession().then(function (session) {
+	var id = session.id_;
+	var SauceLabs = require("saucelabs");
+	var saucelabs = new SauceLabs({
+		username: USER_NAME,
+		password: ACCESS_KEY
+	}); 
+	driver.get(TEST_URL);
+	console.log("Starting tests ...");
+	driver.getTitle().then(function (title) {
+		console.log("title is: " + title);
+		var passed = title === TITLE;
+		var done = function () {
+			console.log("done: " + passed);
+		};
+		//console.log(id);
+		driver.quit();
+			saucelabs.updateJob(id, {
+			name: title,
+			passed: passed
+		}, done);
+	});
+});
+
+
